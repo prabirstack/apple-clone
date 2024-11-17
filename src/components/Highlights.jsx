152081716a94827b286ad0049893073655071fc8
Prabir <prabirsacademy@gmail.com>
2024-11-18 02:55:30 +0530

    hightlights section incomplete

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,43 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+import VideoCarosel from "./VideoCarosel";
+
+const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen h-full overflow-hidden common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="items-center justify-between w-full mb-12 lg:items-end md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+        <VideoCarosel />
+      </div>
+    </section>
+  );
+};
+export default Highlights;
