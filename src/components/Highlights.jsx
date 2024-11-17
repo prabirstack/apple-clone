11d461ef1e52c9fcd43592d2dcc617d603a6cacc
Prabir <prabirsacademy@gmail.com>
2024-11-18 04:55:22 +0530

    highlights section done

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,28 +1,21 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { rightImg, watchImg } from "../utils";
-import VideoCarosel from "./VideoCarosel";
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
+
+
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      stagger: 0.25,
-    });
-  }, []);
+    gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+  }, [])
+
   return (
-    <section
-      id="highlights"
-      className="w-screen h-full overflow-hidden common-padding bg-zinc"
-    >
+    <section id="highlights" className="w-screen h-full overflow-hidden common-padding bg-zinc">
       <div className="screen-max-width">
-        <div className="items-center justify-between w-full mb-12 lg:items-end md:flex">
-          <h1 id="title" className="section-heading">
-            Get the highlights.
-          </h1>
+        <div className="items-end justify-between w-full mb-12 md:flex">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
 
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
@@ -35,9 +28,11 @@ const Highlights = () => {
             </p>
           </div>
         </div>
-        <VideoCarosel />
+
+        <VideoCarousel />
       </div>
     </section>
-  );
-};
-export default Highlights;
+  )
+}
+
+export default Highlights
